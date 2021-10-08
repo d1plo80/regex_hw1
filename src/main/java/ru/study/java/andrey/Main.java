047fdd659195fddbdc20ebe314a16ea83a136859
Andrey <ryazanov.aa@yandex.ru>
2021-10-08 15:51:51 +0300

    remove unused imports

--- src/main/java/ru/study/java/andrey/Main.java
+++ src/main/java/ru/study/java/andrey/Main.java
@@ -3,7 +3,6 @@ package ru.study.java.andrey;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
