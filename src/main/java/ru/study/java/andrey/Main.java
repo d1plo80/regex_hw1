9d1aa1364555bfbac1f4982a3a19f6a6363d03ac
Andrey <ryazanov.aa@yandex.ru>
2021-10-08 15:41:42 +0300

    init project

---
+++ src/main/java/ru/study/java/andrey/Main.java
@@ -0,0 +1,32 @@
+package ru.study.java.andrey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            String str = reader.readLine();
+            Pattern url = Pattern.compile("(\\?|\\&)(([^=]+)\\=([^&]+))");
+            Matcher matcher = url.matcher(str);
+
+            while (matcher.find()) {
+                System.out.println(matcher.group(2));
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+//            int idx = str.indexOf("?");
+//            String strNew = str.substring(idx + 1);
+//            String[] temp = strNew.split("&");
+//            System.out.println(Arrays.toString(temp));
+//            "http://mail.ru?param1=value1&param2=value2"
